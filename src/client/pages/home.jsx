119beca989f9bc739cd01c5920610035649ca6c8
SAMtheo <theobald_sam@network.lilly.com>
2018-06-01 16:10:27 +0100

    enhancement: started header

--- src/client/pages/home.jsx
+++ src/client/pages/home.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Grid, Icon } from '@material-ui/core';
 
 export default () => {
   return (
-    <h1>it is working</h1>
+    <Grid container spacing={0}>
+      <Grid item xs={12}>
+        <div className="Header">
+          <div className="Header-menuButton">
+            <Icon classNAme="material-icons">menu</Icon>
+          </div>
+        </div>
+      </Grid>
+    </Grid>
   );
 }
--- src/client/router.jsx
+++ src/client/router.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import reactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import Home from './pages/home.jsx';
 
+const theme = createMuiTheme();
+
 const app = (
-  <Router>
-    <Route exact path="/" component={Home} />
-  </Router>
+  <MuiThemeProvider theme={theme}>
+    <Router>
+      <Route exact path="/" component={Home} />
+    </Router>
+  </MuiThemeProvider>
 );
 
-reactDOM.render(app, document.getElementById('main'));
+reactDOM.render(app, document.getElementById('app'));
